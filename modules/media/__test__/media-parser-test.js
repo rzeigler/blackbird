d40ee33ca2a2e0e525808c7be3ceabd261416cec
Ryan Zeigler <ryan.zeigler@taillight.com>
2016-07-07 15:18:59 -0500

    Add media parser.

---
+++ modules/media/__test__/media-parser-test.js
@@ -0,0 +1,50 @@
+const assert = require("assert");
+
+const parser = require("../media-parser");
+const media = require("../media");
+
+describe("media-parser", () => {
+    describe("#mediaType", () => {
+        it("should recognize mime types", () => {
+            assert.deepEqual(parser.mediaType.parse("application/json"), {
+                status: true,
+                value: media.media("application", "json", {})
+            });
+        });
+        it("should recognize parameterized media types", () => {
+            assert.deepEqual(parser.mediaType.parse("application/json; q=1; b=r"), {
+                status: true,
+                value: media.media("application", "json", {q: "1", b: "r"})
+            });
+        });
+        it("should recognize vendor types", () => {
+            assert.deepEqual(parser.mediaType.parse("application/vnd.company.user"), {
+                status: true,
+                value: media.media("application", "vnd.company.user", {})
+            });
+        });
+        it("should recognize parameterized vendor types", () => {
+            assert.deepEqual(parser.mediaType.parse("application/vnd.company.user; q=1"), {
+                status: true,
+                value: media.media("application", "vnd.company.user", {q: "1"})
+            });
+        });
+    });
+    describe("#accept", () => {
+        it("should recognize a list of media types", () => {
+            assert.deepEqual(parser.accept.parse("text/plain, text/html"), {
+                status: true,
+                value: [media.media("text", "plain", {}), media.media("text", "html", {})]
+            });
+        });
+        it("should recognize a list of parameterized media types", () => {
+            assert.deepEqual(parser.accept.parse("application/json; encoding=utf8, application/xml; xmlns=a"), {
+                status: true,
+                value: [
+                    media.media("application", "json", {encoding: "utf8"}),
+                    media.media("application", "xml", {xmlns: "a"})
+                ]
+            });
+        });
+    });
+});
