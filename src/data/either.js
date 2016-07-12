93fb4b2442139518d0fe909cea24292bc9a593f8
Ryan Zeigler <zeiglerr@gmail.com>
2016-07-12 15:59:27 -0500

    Add attempt.

--- src/data/either.js
+++ src/data/either.js
@@ -4,9 +4,17 @@ const Either = require("fantasy-eithers");
 const left = Either.Left;
 const right = Either.Right;
 const inhabitOneOf = curry((l, r) => r ? right(r) : left(l));
+const attempt = (curry(f, v) => {
+    try {
+        return new Either.Right(f(v));
+    } catch (e) {
+        return new Either.Left(e);
+    }
+});
 
 module.exports = {
     left,
     right,
-    inhabitOneOf
+    inhabitOneOf,
+    attempt
 };
