195ca05fa28d6fc3feac52467c19cc275df36a01
Ryan Zeigler <zeiglerr@gmail.com>
2016-07-11 20:17:32 -0500

    Readability fiddles.

--- src/data/lens.js
+++ src/data/lens.js
@@ -1,10 +1,10 @@
-const {curry, lens, assoc, dissoc} = require("ramda");
+const {curry, lens, assoc, dissoc, __} = require("ramda");
 const option = require("./option");
 
-const assocLens = (key) => lens(
-        (s) => option.inhabit(s[key]),
-        curry((vOpt, s) =>
-            vOpt.fold((v) => assoc(key, v, s),
-                      () => dissoc(key, s))));
+const assocLens = function (key) {
+    return lens((s) => option.inhabit(s[key]),
+                curry((vOpt, s) => vOpt.fold(assoc(key, __, s),
+                                             () => dissoc(key, s))));
+};
 
 module.exports = {assocLens};
