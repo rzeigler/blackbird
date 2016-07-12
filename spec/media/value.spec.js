ccc1e22b1eb13e5c2bce2ab87e78c92aeec16ed3
Ryan Zeigler <zeiglerr@gmail.com>
2016-07-12 14:55:07 -0500

    Add tests for generalization of.

--- spec/media/value.spec.js
+++ spec/media/value.spec.js
@@ -1,4 +1,6 @@
 const {expect} = require("chai");
+const R = require("ramda");
+const jsverify = require("jsverify");
 const Option = require("fantasy-options");
 const {view} = require("ramda");
 const {parallel} = require("../loader");
@@ -31,6 +33,35 @@ describe("media/value", function () {
         it("should return a none if the parameter is not present", function () {
             expect(view(value.parameterLens("q"), value("application", "json", {})))
                 .to.deep.equal(Option.None);
+        });
+    });
+    describe("#generalizationOf", function () {
+        it("property - generalization of wildcard to all media types", function () {
+            jsverify.assert(jsverify.forall(jsverify.string, jsverify.string, function (t, st) {
+                return value.generalizationOf(value("*", "*", {}), value(t, st, {}));
+            }));
+        });
+        it("property - wildcard cannot be a generalization of anything", function () {
+            jsverify.assert(jsverify.forall(jsverify.string, jsverify.string, function (t, st) {
+                return !value.generalizationOf(value(t, st, {}), value("*", "*", {}));
+            }));
+        });
+        it("property - generalization of subtype wildcard to related media types", function () {
+            jsverify.assert(jsverify.forall(jsverify.string, function (st) {
+                return value.generalizationOf(value("application", "*", {}), value("application", st, {}));
+            }));
+        });
+        it("property - subtype wildcard cannot be a generalization of related", function () {
+            jsverify.assert(jsverify.forall(jsverify.string, function (st) {
+                return !value.generalizationOf(value("application", st, {}), value("application", "*", {}));
+            }));
+        });
+        it("property - generalization of increasing parameter specifialization", function () {
+            jsverify.assert(jsverify.forall(jsverify.nearray(jsverify.nestring), function (params) {
+                const limited = R.drop(1, params);
+                return value.generalizationOf(value("application", "json", R.fromPairs(R.zip(limited, limited))),
+                                              value("application", "json", R.fromPairs(R.zip(params, params))));
+            }));
         });
     });
 });
