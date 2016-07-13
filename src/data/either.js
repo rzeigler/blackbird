55a8197207b678d9a40676814dadbe3772f72582
Ryan Zeigler <zeiglerr@gmail.com>
2016-07-12 19:33:03 -0500

    Flesh out data.either and add tests.

--- src/data/either.js
+++ src/data/either.js
@@ -1,20 +1,22 @@
-const {curry} = require("ramda");
+const {curry, identity} = require("ramda");
 const Either = require("fantasy-eithers");
 
 const left = Either.Left;
 const right = Either.Right;
 const inhabitOneOf = curry((l, r) => r ? right(r) : left(l));
-const attempt = (curry(f, v) => {
+const attempt = curry((f, v) => {
     try {
         return new Either.Right(f(v));
     } catch (e) {
         return new Either.Left(e);
     }
 });
+const leftMap = curry((f, e) => e.bimap(f, identity));
 
 module.exports = {
     left,
     right,
     inhabitOneOf,
-    attempt
+    attempt,
+    leftMap
 };
