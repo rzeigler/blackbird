e7fabba96e00d931ef32dfca84f388b9222edd2b
Ryan Zeigler <zeiglerr@gmail.com>
2016-07-18 14:57:17 -0500

    Add another simple integration test.

--- integration/core.spec.js
+++ integration/core.spec.js
@@ -1,25 +1,45 @@
 const {expect} = require("chai");
 const request = require("request-promise");
 const serve = require("../src/core/serve");
+const Promise = require("bluebird");
 
 /* eslint no-process-env: 0 */
 const port = parseInt(process.env.PORT || "8888", 10);
 const host = `http://localhost:${port}`;
 
 describe("core/serve", function () {
-    let server = null;
-    beforeEach(function () {
-        server = serve.serve(port, function () {
-            return "Hello, World!";
+    describe("simplest possible", function () {
+        let server = null;
+        beforeEach(function () {
+            server = serve.serve(port, function () {
+                return "Hello, World!";
+            });
+        });
+
+        afterEach(function () {
+            server.close();
+        });
+
+        it("should response with hello world", function () {
+            return request(host)
+                .then((text) => expect(text).to.equal("Hello, World!"));
         });
     });
+    describe("simplest possible promised", function () {
+        let server = null;
+        beforeEach(function () {
+            server = serve.serve(port, function () {
+                return Promise.resolve("Hello, World!").delay(500);
+            });
+        });
 
-    afterEach(function () {
-        server.close();
-    });
+        afterEach(function () {
+            server.close();
+        });
 
-    it("should response with hello world", function () {
-        return request(host)
-            .then((text) => expect(text).to.equal("Hello, World!"));
+        it("should response with hello world", function () {
+            return request(host)
+                .then((text) => expect(text).to.equal("Hello, World!"));
+        });
     });
 });
