0e506f582d9915d449625b1f93547e1b62874c69
Ryan Zeigler <zeiglerr@gmail.com>
2016-07-20 22:00:52 -0500

    Untested router implementation.

--- spec/core/message.spec.js
+++ spec/core/message.spec.js
@@ -1,125 +1,140 @@
 const {expect} = require("chai");
 const {parallel} = require("../loader");
+const R = require("ramda");
 const message = parallel(require, __filename);
 
-describe("core/message", function () {
-    describe("#inflateBufferBody", function () {
-        it("should inflate a response structure from a buffer", function () {
-            const buffer = Buffer.from("abcdefg");
-            const result = message.inflateBufferBody(buffer);
-            expect(result.statusCode).to.equal(200);
-            expect(result.headers).to.eql({"content-type": "application/octet-stream"});
-            expect(result.body).to.equal(buffer);
+describe("core", function () {
+    describe("message", () => {
+        describe("urlStruct", () => {
+            it("should decompose paths", function () {
+                expect(message.urlStruct("/a/b/c?q=1&b="))
+                    .to.eql({path: "/a/b/c", pathSplit: ["a", "b", "c"], query: {q: "1", b: ""}});
+            });
         });
-    });
-    describe("#inflateStringBody", function () {
-        it("should inflate a response structure from a string", function () {
-            const inflated = message.inflateStringBody("abcdefg");
-            expect(inflated.statusCode).to.equal(200);
-            expect(inflated.headers).to.eql({"content-type": "text/plain; charset=utf-8"});
-            expect(inflated.body).to.eql(Buffer.from("abcdefg"));
+        describe("overContextParams", () => {
+            it("should over the params", () => {
+                expect(message.overContextParams(R.merge({a: 1}), {params: {b: 2}}))
+                    .to.eql({params: {a: 1, b: 2}});
+            });
         });
-    });
-    describe("#isStringMap", function () {
-        it("should return true for empty objects", function () {
-            expect(message.isStringMap({})).to.equal(true);
+        describe("#inflateBufferBody", function () {
+            it("should inflate a response structure from a buffer", function () {
+                const buffer = Buffer.from("abcdefg");
+                const result = message.inflateBufferBody(buffer);
+                expect(result.statusCode).to.equal(200);
+                expect(result.headers).to.eql({"content-type": "application/octet-stream"});
+                expect(result.body).to.equal(buffer);
+            });
         });
-        it("should return true for object with string properties", function () {
-            expect(message.isStringMap({a: "a", b: "b"})).to.equal(true);
+        describe("#inflateStringBody", function () {
+            it("should inflate a response structure from a string", function () {
+                const inflated = message.inflateStringBody("abcdefg");
+                expect(inflated.statusCode).to.equal(200);
+                expect(inflated.headers).to.eql({"content-type": "text/plain; charset=utf-8"});
+                expect(inflated.body).to.eql(Buffer.from("abcdefg"));
+            });
         });
-        it("should return false for non-'objects'", function () {
-            expect(message.isStringMap("b")).to.equal(false);
+        describe("#isStringMap", function () {
+            it("should return true for empty objects", function () {
+                expect(message.isStringMap({})).to.equal(true);
+            });
+            it("should return true for object with string properties", function () {
+                expect(message.isStringMap({a: "a", b: "b"})).to.equal(true);
+            });
+            it("should return false for non-'objects'", function () {
+                expect(message.isStringMap("b")).to.equal(false);
+            });
+            it("should return false for objects with non-string properties", function () {
+                expect(message.isStringMap({a: "a", b: 8})).to.equal(false);
+            });
         });
-        it("should return false for objects with non-string properties", function () {
-            expect(message.isStringMap({a: "a", b: 8})).to.equal(false);
-        });
-    });
-    describe("#isConformingResponse", function () {
-        it("should return true for a valid response with headers", function () {
-            expect(message.isConformingResponse({
-                statusCode: 200,
-                headers: {"content-type": "text/plain"},
-                body: Buffer.from("Hello, World!")
-            })).to.equal(true);
-        });
-        it("should return true for a valid response without a body", function () {
-            expect(message.isConformingResponse({
-                statusCode: 200,
-                headers: {},
-                body: null
-            })).to.equal(true);
-        });
-        it("should return true for a valid response without headers", function () {
-            expect(message.isConformingResponse({
-                statusCode: 200,
-                body: Buffer.from("Hello, World!")
-            })).to.equal(true);
-        });
-        it("should return true for a valid response with empty headers", function () {
-            expect(message.isConformingResponse({
-                statusCode: 200,
-                headers: {},
-                body: Buffer.from("Hello, World!")
-            })).to.equal(true);
-        });
-        it("should return false for a response without a statusCode", function () {
-            expect(message.isConformingResponse({body: Buffer.from("Hello, World!")}))
-                .to.equal(false);
-        });
-        it("should return false for a response with random headers", function () {
-            expect(message.isConformingResponse({
-                statusCode: 200,
-                body: Buffer.from("Hello, World!"),
-                headers: {a: 5}
-            })).to.equal(false);
+        describe("#isConformingResponse", function () {
+            it("should return true for a valid response with headers", function () {
+                expect(message.isConformingResponse({
+                    statusCode: 200,
+                    headers: {"content-type": "text/plain"},
+                    body: Buffer.from("Hello, World!")
+                })).to.equal(true);
+            });
+            it("should return true for a valid response without a body", function () {
+                expect(message.isConformingResponse({
+                    statusCode: 200,
+                    headers: {},
+                    body: null
+                })).to.equal(true);
+            });
+            it("should return true for a valid response without headers", function () {
+                expect(message.isConformingResponse({
+                    statusCode: 200,
+                    body: Buffer.from("Hello, World!")
+                })).to.equal(true);
+            });
+            it("should return true for a valid response with empty headers", function () {
+                expect(message.isConformingResponse({
+                    statusCode: 200,
+                    headers: {},
+                    body: Buffer.from("Hello, World!")
+                })).to.equal(true);
+            });
+            it("should return false for a response without a statusCode", function () {
+                expect(message.isConformingResponse({body: Buffer.from("Hello, World!")}))
+                    .to.equal(false);
+            });
+            it("should return false for a response with random headers", function () {
+                expect(message.isConformingResponse({
+                    statusCode: 200,
+                    body: Buffer.from("Hello, World!"),
+                    headers: {a: 5}
+                })).to.equal(false);
 
-            expect(message.isConformingResponse({
-                statusCode: 200,
-                body: Buffer.from("Hello, World!"),
-                headers: "a"
-            })).to.equal(false);
+                expect(message.isConformingResponse({
+                    statusCode: 200,
+                    body: Buffer.from("Hello, World!"),
+                    headers: "a"
+                })).to.equal(false);
+            });
+            it("should return false for a response with a body that is not buffer", function () {
+                expect(message.isConformingResponse({statusCode: 200, body: "rekt!"}))
+                    .to.equal(false);
+            });
+            it("should return false for a response with a random statusCode", function () {
+                expect(message.isConformingResponse({statusCode: "a", body: Buffer.from("rekt!")}))
+                    .to.equal(false);
+            });
         });
-        it("should return false for a response with a body that is not buffer", function () {
-            expect(message.isConformingResponse({statusCode: 200, body: "rekt!"}))
-                .to.equal(false);
-        });
-        it("should return false for a response with a random statusCode", function () {
-            expect(message.isConformingResponse({statusCode: "a", body: Buffer.from("rekt!")}))
-                .to.equal(false);
-        });
-    });
-    describe("#inflateResponse", function () {
-        it("should inflate buffers", function () {
-            expect(message.inflateResponse(Buffer.from("boo!")))
-                .to.eql({
+        describe("#inflateResponse", function () {
+            it("should inflate buffers", function () {
+                expect(message.inflateResponse(Buffer.from("boo!")))
+                    .to.eql({
+                        statusCode: 200,
+                        headers: {"content-type": "application/octet-stream"},
+                        body: Buffer.from("boo!")
+                    });
+            });
+            it("should inflate strings", function () {
+                expect(message.inflateResponse("boo!"))
+                    .to.eql({
+                        statusCode: 200,
+                        headers: {"content-type": "text/plain; charset=utf-8"},
+                        body: Buffer.from("boo!")
+                    });
+            });
+            it("should accept conforming response", function () {
+                const payload = {
                     statusCode: 200,
                     headers: {"content-type": "application/octet-stream"},
                     body: Buffer.from("boo!")
-                });
+                };
+                expect(message.inflateResponse(payload))
+                    .to.eql(payload);
+            });
         });
-        it("should inflate strings", function () {
-            expect(message.inflateResponse("boo!"))
-                .to.eql({
-                    statusCode: 200,
-                    headers: {"content-type": "text/plain; charset=utf-8"},
-                    body: Buffer.from("boo!")
-                });
-        });
-        it("should accept conforming response", function () {
-            const payload = {
-                statusCode: 200,
-                headers: {"content-type": "application/octet-stream"},
-                body: Buffer.from("boo!")
-            };
-            expect(message.inflateResponse(payload))
-                .to.eql(payload);
-        });
-    });
-    describe("#conditionResponse", function () {
-        it("should attach content-length when there is a body", function () {
-            const body = message.bufferFromUtf8("Hello, World");
-            expect(message.conditionResponse(message.response(200, {}, body)).headers["content-length"])
-                .to.equal(Buffer.byteLength(body).toString());
+        describe("#conditionResponse", function () {
+            it("should attach content-length when there is a body", function () {
+                const body = message.bufferFromUtf8("Hello, World");
+                expect(message.conditionResponse(message.response(200, {}, body)).headers["content-length"])
+                    .to.equal(Buffer.byteLength(body).toString());
+            });
         });
     });
 });
