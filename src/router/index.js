36bb20860f7ddc1ed4fff45402ad3539ef87b1a3
Ryan Zeigler <zeiglerr@gmail.com>
2016-07-21 15:57:14 -0500

    Implement router tests.

--- src/router/index.js
+++ src/router/index.js
@@ -8,19 +8,22 @@ const router = R.curry((index, paths, context) => {
         return Promise.resolve(msg.response(404, {}, ""));
     }
     const path = R.nth(index, paths);
-    const result = p.match(path.elems, context.unconsumedPathSplit || context.pathSplit);
+    const result = p.match(path.elems, context.remainingPathSplit || context.pathSplit);
     return result.cata({
         Some: (r) => {
             if (R.length(r.remaining) > 0 && !path.isMount) {
                 return router(index + 1, paths, context);
             }
             // Attach unmatched parts to the context for passing along
-            const assocRemaining = R.assoc("unconsumedPathSplit", r.remaining);
+            const assocRemaining = R.assoc("remainingPathSplit", r.remaining);
             const conditionContext = R.compose(msg.overContextParams(R.merge(r.params)), assocRemaining);
-            return path.app(conditionContext);
+            return path.app(conditionContext(context));
         },
         None: () => router(index + 1, paths, context)
     });
 });
 
-module.exports = R.merge({router: router(0)}, require("./path"));
+module.exports = {
+    router: router(0),
+    path: require("./path")
+};
