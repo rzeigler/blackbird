fb7b367e29fa5e801b78470388689ad8a9f7b41f
Ryan Zeigler <zeiglerr@gmail.com>
2016-07-26 08:16:24 -0500

    Comment things broke the build.

--- integration/router.spec.js
+++ integration/router.spec.js
@@ -1,23 +1,23 @@
-const {expect} = require("chai");
-const jsverify = require("jsverify");
-const request = require("request-promise");
-const {serve, context, response, body} = require("../src/core");
-const {router, path} = require("../src/router");
-const Promise = require("bluebird");
-const R = require("ramda");
-
-/* eslint no-process-env: 0 */
-const port = parseInt(process.env.PORT || "8888", 10);
-const host = `http://localhost:${port}`;
-
-describe("router", () => {
-    beforeEach(() => {
-        let a = 0, b = 0;
-        server = server.serve(port, router([
-
-        ]));
-    });
-    describe("routing & dispatching", () => {
-
-    });
-});
+// const {expect} = require("chai");
+// const jsverify = require("jsverify");
+// const request = require("request-promise");
+// const {serve, context, response, body} = require("../src/core");
+// const {router, path} = require("../src/router");
+// const Promise = require("bluebird");
+// const R = require("ramda");
+//
+// /* eslint no-process-env: 0 */
+// const port = parseInt(process.env.PORT || "8888", 10);
+// const host = `http://localhost:${port}`;
+//
+// describe("router", () => {
+//     beforeEach(() => {
+//         let a = 0, b = 0;
+//         server = server.serve(port, router([
+//
+//         ]));
+//     });
+//     describe("routing & dispatching", () => {
+//
+//     });
+// });
