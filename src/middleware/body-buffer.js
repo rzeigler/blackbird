44fdf6b228b373759077e3c4578d722c5e5e21bc
Ryan Zeigler <zeiglerr@gmail.com>
2016-08-13 12:12:03 -0500

    Add middleware example.

--- src/middleware/body-buffer.js
+++ src/middleware/body-buffer.js
@@ -3,9 +3,9 @@ const {
     context: {contentLens},
     body: {buffer}
 } = require("../core");
-const {pre} = require("./index");
+const {pre} = require("./combinators");
 
 const attachBody = R.curry((ctx, buf) => R.merge(ctx, R.objOf("body", buf)));
 
 module.exports = pre((ctx) => R.view(contentLens, ctx).consumeContent(buffer)
-        .then(attachBody(ctx)));
+        .then((buf) => buf.length > 0 ? attachBody(ctx, buf) : ctx));
