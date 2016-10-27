215d706d8acecf8aef94be311d76fb6f0737d12b
Ryan Zeigler <zeiglerr@gmail.com>
2016-10-27 05:37:08 -0500

    Reenable integration tests.
    Currently failing.

--- integration/negotiator.spec.js
+++ integration/negotiator.spec.js
@@ -1,32 +1,42 @@
-// const {expect} = require("chai");
-// const request = require("request-promise");
-// const {serve, response} = require("../src/core");
-// const {negotiators, responder, codecs} = require("../src/negotiator");
-// const Promise = require("bluebird");
-// const R = require("ramda");
-//
-// /* eslint no-process-env: 0 */
-// const port = parseInt(process.env.PORT || "8888", 10);
-// const host = `http://localhost:${port}`;
-//
-// describe("negotiator", () => {
-//     let server = null;
-//     beforeEach(() =>
-//         serve(port, negotiator([
-//             responder(codecs.jsonDecoder, codecs.plainTextEncoder, R.identity),
-//             responder(codecs.plainTextDecoder, codecs.jsonEncoder, R.identity)
-//         ]))
-//         .then((srv) => {
-//             server = srv;
-//         })
-//     );
-//
-//     afterEach(function () {
-//         server.close();
-//     });
-//
-//     it("should return plain text when json is submitted", function () {
-//         return request(host)
-//             .then((text) => expect(text).to.equal("Hello, World!"));
-//     });
-// });
+const R = require("ramda");
+const Promise = require("bluebird");
+const {expect} = require("chai");
+const request = require("request-promise");
+const {serve, response} = require("../src/core");
+const {negotiator, responder, codecs} = require("../src/negotiator");
+
+/* eslint no-process-env: 0 */
+const port = parseInt(process.env.PORT || "8888", 10);
+const host = `http://localhost:${port}`;
+
+describe("negotiator", () => {
+    let server = null;
+    beforeEach(() =>
+        serve(port, negotiator([
+            responder(codecs.jsonDecoder, codecs.plainTextEncoder,
+                (ctx) => Promise.resolve(response.response(200, {}, ctx.body.message))),
+            responder(codecs.plainTextDecoder, codecs.jsonEncoder,
+                (ctx) => Promise.resolve(response.respnose(200, {}, {message: ctx.body})))
+        ]))
+        .then((srv) => {
+            server = srv;
+        })
+    );
+
+    afterEach(function () {
+        server.close();
+    });
+
+    it("should return plain text when json is submitted", function () {
+        console.log("executing test");
+        request({uri: host, body: "hello", headers: {"Content-Type": "text/plain", Accept: "application/json"}})
+            .then((text) => {
+                // console.log(text);
+                expect(text).to.equal("{\"message\": \"hello\"}");
+            })
+            .catch((e) => {
+                // console.error(e);
+                expect(true).to.equal(false);
+            });
+    });
+});
