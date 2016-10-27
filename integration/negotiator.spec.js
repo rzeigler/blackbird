2d1443234cc22e8edb510d0a8f97f854e2e74264
Ryan Zeigler <zeiglerr@gmail.com>
2016-10-27 09:05:25 -0500

    Complete simple integration cases for negotiator.

--- integration/negotiator.spec.js
+++ integration/negotiator.spec.js
@@ -9,47 +9,60 @@ const port = parseInt(process.env.PORT || "8888", 10);
 const host = `http://localhost:${port}`;
 
 describe("negotiator", () => {
-    let server = null;
-    beforeEach((done) => {
-        serve(port, negotiator([
-            responder(codecs.jsonDecoder, codecs.plainTextEncoder,
-                (ctx) => Promise.resolve(response.response(200, {}, ctx.body.message))),
-            responder(codecs.plainTextDecoder, codecs.jsonEncoder,
-                (ctx) => Promise.resolve(response.response(200, {}, {message: ctx.body})))
-        ]))
-        .then((srv) => {
-            server = srv;
-            done();
-        })
-        .catch(done);
+    describe("simple cases", () => {
+        let server = null;
+        beforeEach((done) => {
+            serve(port, negotiator([
+                responder(codecs.jsonDecoder, codecs.plainTextEncoder,
+                    (ctx) => Promise.resolve(response.response(200, {}, ctx.body.message))),
+                responder(codecs.plainTextDecoder, codecs.jsonEncoder,
+                    (ctx) => Promise.resolve(response.response(200, {}, {message: ctx.body})))
+            ]))
+            .then((srv) => {
+                server = srv;
+                done();
+            })
+            .catch(done);
+        });
+
+        afterEach(function () {
+            server.close();
+        });
+
+        it("should return plain text when json is submitted given accept headers", () =>
+            request({uri: host, body: "hello", headers: {"Content-Type": "text/plain", Accept: "application/json"}})
+                .catch(() => {
+                    expect(true).to.equal(false);
+                })
+                .then((text) => {
+                    expect(text).to.equal(JSON.stringify({message: "hello"}));
+                })
+        );
+
+        it("should return json when plain text is submitted", () =>
+            request({
+                uri: host,
+                body: JSON.stringify({message: "hello"}),
+                headers: {"Content-Type": "application/json", Accept: "text/plain"}
+            })
+                .catch((e) => {
+                    console.log(e);
+                    expect(true).to.equal(false);
+                })
+                .then((text) => {
+                    expect(text).to.equal("hello");
+                })
+        );
+
+        it("should error with 415 for an unspected content-type", () =>
+            request({uri: host, body: "booo", headers: {"Content-Type": "text/tex", Accept: "text/plain"}})
+                .then(() => expect(false).to.equal(true))
+                .catch((e) => expect(e.statusCode).to.equal(415))
+        );
+        it("should error with 406 for an unexpected accept", () =>
+            request({uri: host, body: "booo", headers: {"Content-Type": "text/plain", Accept: "text/tex"}})
+                .then(() => expect(false).to.equal(true))
+                .catch((e) => expect(e.statusCode).to.equal(406))
+        );
     });
-
-    afterEach(function () {
-        server.close();
-    });
-
-    it("should return plain text when json is submitted given accept headers", () =>
-        request({uri: host, body: "hello", headers: {"Content-Type": "text/plain", Accept: "application/json"}})
-            .catch(() => {
-                expect(true).to.equal(false);
-            })
-            .then((text) => {
-                expect(text).to.equal(JSON.stringify({message: "hello"}));
-            })
-    );
-
-    it("should return json when plain text is submitted", () =>
-        request({
-            uri: host,
-            body: JSON.stringify({message: "hello"}),
-            headers: {"Content-Type": "application/json", Accept: "text/plain"}
-        })
-            .catch((e) => {
-                console.log(e);
-                expect(true).to.equal(false);
-            })
-            .then((text) => {
-                expect(text).to.equal("hello");
-            })
-    );
 });
