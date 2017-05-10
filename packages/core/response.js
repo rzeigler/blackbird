5d0db19a4fa84fbe9dbefc45e1512141d1d49a22
Ryan Zeigler <zeiglerr@gmail.com>
2017-05-10 06:33:08 -0500

    Prototype response builders.

--- packages/core/response.js
+++ packages/core/response.js
@@ -1,3 +1,28 @@
 "use strict";
 
-const empty = {};
+const R = require("ramda");
+const L = require("partial.lenses");
+
+const headers = R.compose(L.prop("headers"), L.default({}));
+const header = R.compose(headers, L.prop("name"));
+const status = L.prop("status");
+const body = L.prop("body");
+
+const setHeader = R.curry((name, value) =>
+    L.set(header, value));
+
+const setStatus = (value) => L.set(status, value);
+
+const setBody = (value) => L.set(body, value);
+
+const buildResponse = (ops) => R.reduce(R.compose, R.identity, ops)({});
+
+module.exports = {
+    headers,
+    header,
+    status,
+    setHeader,
+    setStatus,
+    setBody,
+    buildResponse
+};
